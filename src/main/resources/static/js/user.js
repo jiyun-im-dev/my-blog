c776a086d3aef65c288e9fab4edd0801592024c8
jiyun.im.dev <jiyun.im.dev@gmail.com>
2024-01-08 19:25:07 +0900

    회원가입 자바스크립트에서 컨트롤러 클래스로 요청을 전달하도록 수정

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -14,8 +14,20 @@ let userObject = {
             password: $("#pwd").val(),
             email: $("#email").val()
         }
-        console.log(user);
-        // TODO: 유저 컨트롤러로 전달하는 로직 추가
+
+        // Ajax를 이용한 비동기 호출
+        $.ajax({
+            type: "POST",
+            url: "/auth/insertUser", // 요청 경로
+            data: JSON.stringify(user), // user 객체를 JSON 형식 문자열로 변환
+            // HTTP의 body에 설정되는 데이터 MIME(파일 변환) 타입
+            contentType: "application/json; charset=utf-8",
+        }).done(function (response) { // 응답으로 들어온 JSON 데이터를 response로 받음
+            console.log(response);
+            location = "/";
+        }).fail(function (error) {
+            alert("에러 발생: " + error);
+        });
     },
 }
 
