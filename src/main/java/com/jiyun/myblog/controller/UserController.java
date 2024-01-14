ac38d39fd64556b5f8b86e8ae131d935c3d14b37
jiyun.im.dev <jiyun.im.dev@gmail.com>
2024-01-14 11:54:57 +0900

    유저 컨트롤러 클래스가 서비스 클래스를 사용하도록 수정

--- src/main/java/com/jiyun/myblog/controller/UserController.java
+++ src/main/java/com/jiyun/myblog/controller/UserController.java
@@ -1,14 +1,32 @@
 package com.jiyun.myblog.controller;
 
+import com.jiyun.myblog.domain.User;
+import com.jiyun.myblog.dto.ResponseDTO;
+import com.jiyun.myblog.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserController {
 
+	@Autowired
+	private UserService userService;
+
 	@GetMapping("/auth/insertUser")
 	public String insertUser() {
 		return "/user/insertUser";
+	}
+
+	@PostMapping("/auth/insertUser")
+	public @ResponseBody ResponseDTO<?> insertUser(@RequestBody User user) {
+		userService.insertUser(user);
+		return new ResponseDTO<>(HttpStatus.OK.value(),
+				user.getUsername() + "님 회원가입이 완료되었습니다.");
 	}
 
 }
